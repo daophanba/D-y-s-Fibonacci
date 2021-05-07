629c702ba44a72dd07435cfeff9d0fef35f3c059
daophanba <83689481+daophanba@users.noreply.github.com>
2021-05-07 12:24:55 +0700

    Dãy số Fibonacci

---
+++ Dãy số Fibonacci/ConsoleApp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace VietTutsCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i;
+            Console.Write("100 so dau tien cua day so Fibonacci: \n");
+            for (i = 0; i < 100; i++)
+            {
+                Console.Write("{0} ", fibonacci(i));
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+
+        
+        static int fibonacci(int n)
+        {
+            int f0 = 0;
+            int f1 = 1;
+            int fn = 1;
+            int i;
+
+            if (n < 0)
+            {
+                return -1;
+            }
+            else if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                for (i = 2; i < n; i++)
+                {
+                    f0 = f1;
+                    f1 = fn;
+                    fn = f0 + f1;
+                }
+            }
+            return fn;
+        }
+    }
+}
